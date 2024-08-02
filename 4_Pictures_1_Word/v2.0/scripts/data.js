00d8726693f4e844e49aeb5ad085f1d2639ab4ad
ShkodranH <shkodi.hasani@gmail.com>
2024-08-02 23:10:52 +0200

    added version 2

--- 4_Pictures_1_Word/v2.0/scripts/data.js
+++ 4_Pictures_1_Word/v2.0/scripts/data.js
@@ -1,13 +1,94 @@
 export const levelsData = [
     {
         level: 1,
-        pictures: ['ball-1.png', 'ball-2.png', 'ball-3.png', 'ball-4.png'],
-        answer: "ball"
+        pictures: ['book-1.png', 'book-2.png', 'book-3.png', 'book-4.png'],
+        answer: 'book'
     },
     {
         level: 2,
-        pictures: ['book-1.png', 'book-2.png', 'book-3.png', 'bpp']
-    }
+        pictures: ['green-1.png', 'green-2.png', 'green-3.png', 'green-4.png'],
+        answer: 'green'
+    },
+    {
+        level: 3,
+        pictures: ['chest-1.png', 'chest-2.png', 'chest-3.png', 'chest-4.png'],
+        answer: 'chest'
+    },
+    {
+        level: 4,
+        pictures: ['fan-1.png', 'fan-2.png', 'fan-3.png', 'fan-4.png'],
+        answer: 'fan'
+    },
+    {
+        level: 5,
+        pictures: ['game-1.png', 'game-2.png', 'game-3.png', 'game-4.png'],
+        answer: 'game'
+    },
+    {
+        level: 6,
+        pictures: ['water-1.png', 'water-2.png', 'water-3.png', 'water-4.png'],
+        answer: 'water'
+    },
+    {
+        level: 7,
+        pictures: ['ball-1.png', 'ball-2.png', 'ball-3.png', 'ball-4.png'],
+        answer: 'ball'
+    },
+    {
+        level: 8,
+        pictures: ['net-1.png', 'net-2.png', 'net-3.png', 'net-4.png'],
+        answer: 'net'
+    },
+    {
+        level: 9,
+        pictures: ['date-1.png', 'date-2.png', 'date-3.png', 'date-4.png'],
+        answer: 'date'
+    },
+    {
+        level: 10,
+        pictures: ['vacuum-1.png', 'vacuum-2.png', 'vacuum-3.png', 'vacuum-4.png'],
+        answer: 'vacuum'
+    },
+    {
+        level: 11,
+        pictures: ['fast-1.png', 'fast-2.png', 'fast-3.png', 'fast-4.png'],
+        answer: 'fast'
+    },
+    {
+        level: 12,
+        pictures: ['train-1.png', 'train-2.png', 'train-3.png', 'train-4.png'],
+        answer: 'train'
+    },
+    {
+        level: 13,
+        pictures: ['capture-1.png', 'capture-2.png', 'capture-3.png', 'capture-4.png'],
+        answer: 'capture'
+    },
+    {
+        level: 14,
+        pictures: ['hot-1.png', 'hot-2.png', 'hot-3.png', 'hot-4.png'],
+        answer: 'hot'
+    },
+    {
+        level: 15,
+        pictures: ['old-1.png', 'old-2.png', 'old-3.png', 'old-4.png'],
+        answer: 'old'
+    },
+    {
+        level: 16,
+        pictures: ['drink-1.png', 'drink-2.png', 'drink-3.png', 'drink-4.png'],
+        answer: 'drink'
+    },
+    {
+        level: 17,
+        pictures: ['full-1.png', 'full-2.png', 'full-3.png', 'full-4.png'],
+        answer: 'full'
+    },
+    {
+        level: 18,
+        pictures: ['hard-1.png', 'hard-2.png', 'hard-3.png', 'hard-4.png'],
+        answer: 'hard'
+    },
 ];
-// "./images/apple.png"
-console.log(levelsData[1].pictures)
+// Adding the folder path to each picture src
+levelsData.forEach(el => el.pictures.forEach((e, i, arr) => arr[i] = `./images/${e}`));
--- 4_Pictures_1_Word/v2.0/scripts/index.js
+++ 4_Pictures_1_Word/v2.0/scripts/index.js
@@ -9,8 +9,8 @@ const playBtn = document.querySelector('.play-btn');
 const finishBtn = document.querySelector('.finish-btn');
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-let level = 1;
-let currentLevelData = levelsData[level - 1];
+let levelIndex = 0;
+let currentLevelData = levelsData[levelIndex];
 let currentAnswer = currentLevelData.answer;
 let keyboardKeys = [];
 let currentGuess = [];
@@ -20,34 +20,38 @@ let letterCounter = 0;
 function levelUp(levelNum) {
     currentLevelData = levelsData[levelNum];
     currentAnswer = currentLevelData.answer;
+    levelElem.innerText = `Level ${currentLevelData.level}`;
     letterCounter = 0;
     generateWordField();
     generateKeyboardKeys();
     addPictures();
 }
-levelUp(level);
+levelUp(levelIndex++);
 
-// const clickAudio = new Audio("./sound-effects/click.wav");
-// const wrongAudio = new Audio("./sound-effects/wrong.mp3");
-// const revealAudio = new Audio("./sound-effects/reveal.wav");
-// const winAudio = new Audio("./sound-effects/win.wav");
-// const loseAudio = new Audio("./sound-effects/lose.wav");
+const clickAudio = new Audio("./sound-effects/click.mp3");
+const correctAudio = new Audio("./sound-effects/correct.wav");
+const wrongAudio = new Audio("./sound-effects/wrong.mp3");
+const winAudio = new Audio("./sound-effects/win.mp3");
 
 function changeScene(prev, next) {
     document.querySelector(prev).style.display = 'none';
     document.querySelector(next).style.display = 'flex';
 }
+// Generate HTML elements for the word guess container
 function generateWordField() {
     wordElem.innerHTML = "";
     for(let i of currentAnswer)
         wordElem.innerHTML += "<div></div>";
 }
+// Generate an array with 12 random characters for the keyboar 
+// But should contain the letters for the current answer
 function generateKeyboardKeys() {
     keyboardKeys = Array.from({ length: 12 }, (_, i) => i < currentAnswer.length 
         ? currentAnswer[i] : alphabet[Math.floor(Math.random() * alphabet.length)])
         .sort(() => Math.random() - 0.5);
     keyboardKeyElems.forEach((e, i) => e.innerText = keyboardKeys[i]);
 }
+// Add the file path for the pictures
 function addPictures() {
     pictureElems.forEach((e, i) => {
         e.src = currentLevelData.pictures[i];
@@ -60,59 +64,71 @@ function mouseClick(e) {
     if(e.target.closest('[data-key]')) {
         addLetter(e.target.innerText);
         e.target.classList.add('hidden-key');
+        clickAudio.play();
     }
     else if(e.target.closest('[data-undo]'))
         deleteGuess();
 }
 
+// Update the words current letter and the corresponding HTML element
 function updateWordGuess(index, value) {
     currentGuess[index] = value.toLowerCase();
-    wordElem.children[index].innerText = value;
+    wordElem.childNodes[index].innerText = value;
 }
-// Add the letter inputed to the current guess if it has still space
+// Add the letter clicked to the current guess
 function addLetter(letter) {
-    if(letterCounter < currentAnswer.length)
-        updateWordGuess(letterCounter++, letter);
-    else
+    updateWordGuess(letterCounter++, letter);
+    if(letterCounter == currentAnswer.length)
         checkGuess();
 }
 // Remove all the letters from the guess (undo)
 function deleteGuess() {
-    Array.from(wordElem.children).forEach(e => e.innerText = '');
+    wordElem.childNodes.forEach(e => e.innerText = '');
     keyboardKeyElems.forEach(e => e.classList.remove('hidden-key'));
     currentGuess.length = letterCounter = 0;
 }
-// Check if the current guess is in the word list and mark the letter accordingly
-async function checkGuess() {
-    if(currentGuess.join('') == currentAnswer) {
-        
-        // disabledinputs();
-        // for(let i = 0; i < wordsLength; i++){
-        //     gameboardCells[currentRow][i].classList.add('reveal');
-        //     await new Promise(resolve => setTimeout(resolve, 300));
 
-        //     if(answer[i] === currentGuess[i])
-        //         gameboardCells[currentRow][i].classList.add('green-mark');
-        //     else if(answer.includes(currentGuess[i])) 
-        //         gameboardCells[currentRow][i].classList.add('yellow-mark');
-        //     else
-        //         gameboardCells[currentRow][i].classList.add('gray-mark');
-        // }
-        // enableInputs();
-        levelUp(++level);
+// Check if the current guess is correct or not
+async function checkGuess() {
+    keyboard.removeEventListener('click', mouseClick);
+    gameFinish();
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    if(currentGuess.join('') == currentAnswer) {
+        correctAudio.play();
+        for(let i of wordElem.childNodes) {
+            i.classList.add('correct');
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+        await new Promise(resolve => setTimeout(resolve, 700));
+        wordElem.childNodes.forEach(e => e.classList.remove('correct'));
+        levelUp(levelIndex++);
     }
     else {
-        // gameboardRows[currentRow].classList.add('wrong');
-        // await new Promise(resolve => setTimeout(resolve, 500));
-        // gameboardRows[currentRow].classList.remove('wrong');
-        deleteGuess()
+        wrongAudio.play();
+        wordElem.classList.add('wrong');
+        await new Promise(resolve => setTimeout(resolve, 700));
+        wordElem.classList.remove('wrong');
+    }
+    deleteGuess();
+    keyboard.addEventListener('click', mouseClick);
+}
+// Check if the player has finished all the levels
+async function gameFinish() {
+    if(levelIndex == levelsData.length) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        changeScene('.stage', '.finish');
+        winAudio.play();
     }
 }
 
 playBtn.addEventListener('click', () => {
     changeScene('.intro', '.stage');
 });
-finishBtn.addEventListener('click', async () => {
-    changeScene('.stage', '.intro');
+finishBtn.addEventListener('click', () => {
+    changeScene('.finish', '.intro');
+    levelIndex = 0;
+    levelUp(levelIndex++);
+
 });
 keyboard.addEventListener('click', mouseClick);
