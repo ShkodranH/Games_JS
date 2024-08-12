e9bbc0b61444fe8916866d6239b0bb02900c392f
ShkodranH <shkodi.hasani@gmail.com>
2024-08-12 20:35:15 +0200

    added responsivity

--- Memorize_Cards/v2.0/scripts/index.js
+++ Memorize_Cards/v2.0/scripts/index.js
@@ -1,80 +1,62 @@
 import { imagesArray } from "./data.js";
 
 const levelElem = document.querySelector('.level');
-const pictureElems = document.querySelectorAll('.pictures img');
-const wordElem = document.querySelector('.word');
-const keyboard = document.querySelector('.keyboard');
-const keyboardKeyElems = keyboard.querySelectorAll('[data-key]');
+const currentProgressElem = document.querySelector('.current');
+const overallProgressElem = document.querySelector('.overall');
+// const cardContainerElem = document.querySelector('.card-container');
+const cardElems = document.querySelectorAll('.card');
 const playBtn = document.querySelector('.play-btn');
 const finishBtn = document.querySelector('.finish-btn');
+const finalScoreElem = document.querySelector('.final-score');
 
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-let levelIndex = 0;
-let currentLevelData = levelsData[levelIndex];
-let currentAnswer = currentLevelData.answer;
-let keyboardKeys = [];
-let currentGuess = [];
-let letterCounter = 0;
+let level, progressCount, overallProgress;
+let levelImages = [], currentGuesses = [];
 
 // Reseting variables for new level
-function levelUp(levelNum) {
-    currentLevelData = levelsData[levelNum];
-    currentAnswer = currentLevelData.answer;
-    levelElem.innerText = `Level ${currentLevelData.level}`;
-    letterCounter = 0;
-    generateWordField();
-    generateKeyboardKeys();
-    addPictures();
+function levelUp(value) {
+    level = value;
+    levelElem.innerText = `Level ${level}`;
+    generateCurrentProgress(0);
+    generateOverallProgress(level + 2);
+    generatelevelImages();
 }
-levelUp(levelIndex++);
+levelUp(10);
 
 const clickAudio = new Audio("./sound-effects/click.mp3");
 const correctAudio = new Audio("./sound-effects/correct.wav");
-const wrongAudio = new Audio("./sound-effects/wrong.mp3");
+const loseAudio = new Audio("./sound-effects/lose.mp3");
 const winAudio = new Audio("./sound-effects/win.mp3");
 
 function changeScene(prev, next) {
     document.querySelector(prev).style.display = 'none';
     document.querySelector(next).style.display = 'flex';
 }
+function generateCurrentProgress(value) {
+    progressCount = value;
+    currentProgressElem.innerText = progressCount;
+}
+function generateOverallProgress(value) {
+    overallProgress = value;
+    overallProgressElem.innerText = overallProgress;
+}
+
 // Generate HTML elements for the word guess container
-function generateWordField() {
-    wordElem.innerHTML = "";
-    for(let i of currentAnswer)
-        wordElem.innerHTML += "<div></div>";
+function generatelevelImages() {
+    levelImages = imagesArray.sort(() => Math.random() - 0.5).slice(0, overallProgress);
+    for(let i = 0; i < levelImages.length; i++)
+        cardElems[i].innerHTML = `<img src="${levelImages[i]}">`;
+    console.log(levelImages);
 }
-// Generate an array with 12 random characters for the keyboar 
-// But should contain the letters for the current answer
-function generateKeyboardKeys() {
-    keyboardKeys = Array.from({ length: 12 }, (_, i) => i < currentAnswer.length 
-        ? currentAnswer[i] : alphabet[Math.floor(Math.random() * alphabet.length)])
-        .sort(() => Math.random() - 0.5);
-    keyboardKeyElems.forEach((e, i) => e.innerText = keyboardKeys[i]);
-}
-// Add the file path for the pictures
-function addPictures() {
-    pictureElems.forEach((e, i) => {
-        e.src = currentLevelData.pictures[i];
-        e.onerror = () => {
-            e.src = '../v1.0/images/default.jpg';
-            e.onerror = null;
-        }
-    });
-}
+
 // Handling player click inputs
 function mouseClick(e) {
-    if(e.target.closest('[data-key]')) {
-        addLetter(e.target.innerText);
-        e.target.classList.add('hidden-key');
-        clickAudio.play();
-    }
-    else if(e.target.closest('[data-undo]'))
-        deleteGuess();
+    addLetter(e.target.src);
+    clickAudio.play();
 }
 
 // Update the words current letter and the corresponding HTML element
-function updateWordGuess(index, value) {
-    currentGuess[index] = value.toLowerCase();
+function updateWordGuess(value) {
+    currentGuesses[index] = value.toLowerCase();
     wordElem.childNodes[index].innerText = value;
 }
 // Add the letter clicked to the current guess
@@ -82,12 +64,6 @@ function addLetter(letter) {
     updateWordGuess(letterCounter++, letter);
     if(letterCounter == currentAnswer.length)
         checkGuess();
-}
-// Remove all the letters from the guess (undo)
-function deleteGuess() {
-    wordElem.childNodes.forEach(e => e.innerText = '');
-    keyboardKeyElems.forEach(e => e.classList.remove('hidden-key'));
-    currentGuess.length = letterCounter = 0;
 }
 
 // Check if the current guess is correct or not
@@ -106,7 +82,7 @@ async function checkGuess() {
         gameFinish();
     }
     else {
-        wrongAudio.play();
+        loseAudio.play();
         wordElem.classList.add('wrong');
         await new Promise(resolve => setTimeout(resolve, 700));
         wordElem.classList.remove('wrong');
@@ -116,8 +92,8 @@ async function checkGuess() {
 }
 // Check if the player has finished all the levels
 async function gameFinish() {
-    if(levelIndex < levelsData.length) 
-        levelUp(levelIndex++);
+    if(level < 10) 
+        levelUp(level++);
     else {
         changeScene('.stage', '.finish');
         winAudio.play();
@@ -129,7 +105,6 @@ playBtn.addEventListener('click', () => {
 });
 finishBtn.addEventListener('click', () => {
     changeScene('.finish', '.intro');
-    levelIndex = 0;
-    levelUp(levelIndex++);
+    levelUp(1);
 });
-keyboard.addEventListener('click', mouseClick);
+cardElems.forEach(e => e.addEventListener('click', mouseClick));
