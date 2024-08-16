39eee303d7c60cc2cc71f7b1ddafd94ed6225745
ShkodranH <shkodi.hasani@gmail.com>
2024-08-16 14:14:19 +0200

    adding the functionality

--- Genius_Quiz/v2.0/scripts/data.js
+++ Genius_Quiz/v2.0/scripts/data.js
@@ -13,51 +13,45 @@ export const questionData = [
     },
     {
         number: "3",
-        question: "Which number do we get if we round the PI to the neares integer?",
+        question: "Which number do we get if we round &pi; to the neares integer?",
         options: ["2", "4", "1", "5"],
         answer: "3"
     },
     {
         number: "4",
-        question: "Double of half of 2",
-        options: ["1/2", "4", "1<sup>2</sup>", "2<sup>2</sup>"],
+        question: "Double of half of <span>2</span>",
+        options: ["\u00b9\u2044\u2082", "4", "1\u00b2", "2\u00b2"],
         answer: "2"
     },
     {
         number: "5",
-        question: "1 = 4 <br>2 = 8 <br>3 = 12 <br>4 = ?",
-        options: ["14", "16", "20", "8"],
-        answer: "1"
+        question: "How many natural satellite dose Venus has?",
+        options: ["0", "8", "9", "1"],
+        answer: "0"
     },
     {
         number: "6",
-        question: "A pen and a book cost 110 dollars. <br>The book cost 100 dollars more than pen. <br>How much dose it cost the pen?",
-        options: ["100", "10", "5", "unknown"],
-        answer: "5"
+        question: "A pen and a book cost 110&dollar;. <br>The book cost 100&dollar; more than pen.<br>How much does it cost the pen?",
+        options: ["100\u0024", "10\u0024", "5\u0024", "unknown"],
+        answer: "5\u0024"
     },
     {
         number: "7",
-        question: "Click on True",
-        options: ["(a + b)<sup>2</sup> = a<sup>2</sup> + 2ab + b<sup>2</sup>", "3<sup>3</sup> = 27", "54 ÷ 3 = 18", "14 x 5 = 70"],
+        question: "Click on <span>True</span>",
+        options: ["(a + b)\u00b2 = a\u00b2 + 2ab + b\u00b2", "3\u00b3 = 27", "54 ÷ 3 = 18", "14 x 5 = 70"],
         answer: "True"
-    },
-    {
-        number: "8",
-        question: "You are running in a race with your friends and you just passed the 2nd place.\nIn which position are you?",
-        options: ["1st", "3rd", "last", "unknown"],
-        answer: "2nd"
     },
     {
         number: "",
         question: "Which is the number of this question?",
         options: ["7", "10", "8", "9"],
-        answer: "9"
+        answer: "8"
     },
     {
-        number: "10",
-        question: "How many natural satellite dose Venus has?",
-        options: ["0", "8", "9", "1"],
-        answer: "0"
+        number: "9",
+        question: "You are running in a race with your friends and you just passed the <span>2nd</span> place.<br>In which position are you?",
+        options: ["1st", "3rd", "last", "unknown"],
+        answer: "2nd"
     },
     {
         number: "11",
@@ -73,68 +67,50 @@ export const questionData = [
     },
     {
         number: "13",
-        question: "Which question was skipped?",
-        options: ["7", "8", "2", "11"],
-        answer: "8"
+        question: "<span>1</span> = 4<br>2 = 8<br>&ensp;3 = 12<br>4 = ?&emsp14;",
+        options: ["14", "16", "20", "8"],
+        answer: "1"
     },
     {
         number: "14",
+        question: "Which question was skipped?",
+        options: ["11", "4", "2", "10"],
+        answer: "10"
+    },
+    {
+        number: "15",
         question: "What result will you get, when all the nubers on telephone dial-pad are multiplied?",
         options: ["0", "45", "55", "362880"],
         answer: "0"
     },
     {
-        number: "15",
+        number: "16",
         question: "What mathematical symbol can be put between 5 and 9, to get a value bigger than 5 and smaller than 9?",
         options: ["+", "-", ".", "+"],
         answer: "."
     },
     {
-        number: "16",
+        number: "17",
+        question: "Try your Luck!",
+        options: ["&ensp;", "&ensp;", "&ensp;", "&emsp;"],
+        answer: "&emsp;"
+    },
+    {
+        number: "18",
         question: "Which is the biggest planet in our solar system?",
         options: ["Neptun", "Saturn", "Sun", "Jupiter"],
         answer: "Jupiter"
     },
     {
-        number: "17",
-        question: "Which question had no number?",
-        options: ["14", "12", "9", "11"],
-        answer: "12"
-    },
-    {
-        number: "18",
+        number: "19",
         question: "In a pool lies a flower, the flower doubles in size every day, in 30 day it will cover the pool compeletly, in how many days will the flower cover half of the pool?",
         options: ["2", "15", "29", "16"],
         answer: "29"
     },
     {
-        number: "16",
-        question: "2x + y = 10, x - y = y, 2z = y + 6, z = ?",
-        options: ["5", "3", "0", "2"],
-        answer: "x"
-    },
-    {
-        number: "17",
-        question: "Save the man",
-        options: ["", "", "", ""],
-        answer: "20 click / timer"
-    },
-    {
-        number: "18",
-        question: "How do you enter a door?",
-        options: ["", "", "", ""],
-        answer: "knock knock dbl-click"
-    },
-    {
-        number: "19",
-        question: "Try your Luck!",
-        options: ["", "", "", ""],
-        answer: ""
-    },
-    {
         number: "20",
         question: "Only one chance",
-        options: ["Click", "Don't Click"],
+        options: ["Don't Click", "Click"],
         answer: "Click"
     }
 ];
