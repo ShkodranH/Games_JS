e4b869c2fce6bd0fcd1228e1b468c26d2dd7f44e
ShkodranH <shkodi.hasani@gmail.com>
2025-07-21 19:47:41 +0200

    init the structure folder of the mario game

---
+++ Mario/index.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector('#canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth; // window.innerWidth
+canvas.height = innerHeight;
+class Player {
+    constructor() {
+        this.position = { x: 100, y: 100 }
+        this.velocity = { x: 0, y: 1 }
+        this.width = 30;
+        this.height = 30;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+    }
+}
+
+const player = new Player();
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
+}
+animate();
